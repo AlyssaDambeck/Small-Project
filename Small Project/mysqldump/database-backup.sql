057fbbcb13311b414da1385b7b333dfd78e647af
Alfonso <al570757@ucf.edu>
2026-02-13 15:49:28 -0500

    Update MySQL dump version and database names

--- Small Project/mysqldump/database-backup.sql
+++ Small Project/mysqldump/database-backup.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.45, for Linux (x86_64)
 --
--- Host: localhost    Database: COP4331
+-- Host: localhost    Database: smallProject
 -- ------------------------------------------------------
--- Server version	8.0.30-0ubuntu0.20.04.2
+-- Server version	8.0.45-0ubuntu0.24.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,14 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `COP4331`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `COP4331` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-USE `COP4331`;
-
---
 -- Table structure for table `Contacts`
 --
 
@@ -34,16 +26,22 @@ CREATE TABLE `Contacts` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(50) NOT NULL DEFAULT '',
   `LastName` varchar(50) NOT NULL DEFAULT '',
-  `PhoneNumber` varchar(50) NOT NULL DEFAULT '',
-  `EmailAddress` varchar(50) NOT NULL DEFAULT '',
+  `Phone` varchar(50) NOT NULL DEFAULT '',
+  `Email` varchar(50) NOT NULL DEFAULT '',
   `UserID` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `Contacts`
 --
+
+LOCK TABLES `Contacts` WRITE;
+/*!40000 ALTER TABLE `Contacts` DISABLE KEYS */;
+INSERT INTO `Contacts` VALUES (1,'Rick','Leinecker','111-111-1111','Richard.Leinecker@ucf.edu',2),(2,'Rick','Leinecker','111-111-1111','Richard.Leinecker@ucf.edu',3),(3,'Rick','Leinecker','111-111-1111','Richard.Leinecker@ucf.edu',5),(4,'Knightro','UCF','407-123-1234','knightro.UCF@ucf.edu',1),(5,'Mary','Smith','976-986-3456','Mary.Smith@ucf.edu',3);
+/*!40000 ALTER TABLE `Contacts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Users`
@@ -54,19 +52,24 @@ DROP TABLE IF EXISTS `Users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Users` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `DateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `DateLastLoggedIn` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `FirstName` varchar(50) NOT NULL DEFAULT '',
   `LastName` varchar(50) NOT NULL DEFAULT '',
   `Login` varchar(50) NOT NULL DEFAULT '',
   `Password` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `Users`
 --
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'Rick','Leinecker','RLEINECKER','COP4331'),(2,'Alfonso','Martinez','AMARTINEZ','AM'),(3,'Alyssa','Dambeck','ADAMBECK','AD'),(4,'Grace','Hawkins','GHAWKINS','GH'),(5,'Rachel','Ross','RROSS','RR'),(6,'Wade','Zimmerle','WZIMMERLE','WZ');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -76,4 +79,4 @@ CREATE TABLE `Users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-08-31  4:23:15
+-- Dump completed on 2026-02-13 18:32:55
